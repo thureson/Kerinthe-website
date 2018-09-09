f06ecef60cfd4b01b3069c18081b2ace9300aa73
thureson <tom.thureson@gmail.com>
2018-09-09 23:18:00 +0300

    Try experiemental animations with threeJS

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -3,6 +3,13 @@ import './Content.css';
 
 const Content = ({ children = [] }) =>
   <div className="Content">
+    <div style={
+      { 
+        height: '120vh',
+        marginTop: '350px',
+        maxWidth: '100%',
+        overflow: 'hidden'
+        }} id="three-container" />
     {children}
   </div>
 
